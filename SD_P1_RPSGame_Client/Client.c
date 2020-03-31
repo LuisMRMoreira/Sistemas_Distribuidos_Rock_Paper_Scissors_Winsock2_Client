75cdfc42d7aaf7003a510c4cf71d0f77e91d0318
Luís Miguel Ribeiro Moreira <lmr_moreira@live.com.pt>
2020-03-31 22:14:54 +0100

    Comentários em Português. Limpeza do código.

--- SD_P1_RPSGame_Client/Client.c
+++ SD_P1_RPSGame_Client/Client.c
@@ -8,7 +8,6 @@
 #include <stdio.h>
 
 
-// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
@@ -18,6 +17,7 @@
 #define DEFAULT_PORT "68000"
 
 
+// O IP do servidor a que o cliente se irá conectar é passada como parametro (Project -> SD_PI_RPSGame_Cliente Properties -> Configuration Properties -> Debugging).
 int __cdecl main(int argc, char** argv)
 {
     WSADATA wsaData;
@@ -25,32 +25,37 @@ int __cdecl main(int argc, char** argv)
     struct addrinfo* result = NULL,
         * ptr = NULL,
         hints;
-    char sendbuf[DEFAULT_BUFLEN];
-    char recvbuf[DEFAULT_BUFLEN];
+
+    // Variaveis que contêm informação das mensagens entre o cliente e servidor.
+    char sendbuf[DEFAULT_BUFLEN]; // Buffer com a string que será enviada pelo cliente.
+    char recvbuf[DEFAULT_BUFLEN]; // Buffer com a string recebida pelo cliente.
     int iResult;
     int recvbuflen = DEFAULT_BUFLEN;
     int sendbuflen = DEFAULT_BUFLEN;
+
+    // Booleano que indica a intenção de terminar a conexão com o servidor.
     bool endConnection = 0;
 
-    // Validate the parameters
-    if (argc != 2) {
+    // argumento 1 -> executável ; argumento 2 -> IP. Caso isto não se verifique é porque o número de parametros é inválido.
+    if (argc != 2) { 
         printf("usage: %s server-name\n", argv[0]);
         return 1;
     } 
 
-    // Initialize Winsock
+    // Initialize a utilização da biblioteca Winsock por um proceso.
     iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != 0) {
         printf("WSAStartup failed with error: %d\n", iResult);
         return 1;
     }
 
+    // Configurações dos dados de comunicação.
     ZeroMemory(&hints, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
-    // Resolve the server address and port
+    // Obtem o endereço e a porta do servidor.
     iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result);
     if (iResult != 0) {
         printf("getaddrinfo failed with error: %d\n", iResult);
@@ -58,10 +63,10 @@ int __cdecl main(int argc, char** argv)
         return 1;
     }
 
-    // Attempt to connect to an address until one succeeds
+    // Tenta conectar a um endereço até que um seja bem sucedido
     for (ptr = result; ptr != NULL; ptr = ptr->ai_next) {
 
-        // Create a SOCKET for connecting to server
+        // Criação de um servidor para conectar o cliente ao servidor.
         ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
             ptr->ai_protocol);
         if (ConnectSocket == INVALID_SOCKET) {
@@ -70,7 +75,7 @@ int __cdecl main(int argc, char** argv)
             return 1;
         }
 
-        // Connect to server.
+        // Conectar o cliente ao servidor
         iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
         if (iResult == SOCKET_ERROR) {
             closesocket(ConnectSocket);
@@ -80,65 +85,44 @@ int __cdecl main(int argc, char** argv)
         break;
     }
 
+    // Libertar o espaço de memória ocupado pela informação utilizado para criar o socket
     freeaddrinfo(result);
 
+    // Verificar se o socket é válido
     if (ConnectSocket == INVALID_SOCKET) {
         printf("Unable to connect to server!\n");
         WSACleanup();
         return 1;
     }
 
-    //// Send an initial buffer
-    //iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
-    //if (iResult == SOCKET_ERROR) {
-    //    printf("send failed with error: %d\n", WSAGetLastError());
-    //    closesocket(ConnectSocket);
-    //    WSACleanup();
-    //    return 1;
-    //}
-
-    //printf("Bytes Sent: %ld\n", iResult);
-
-
-    //Antes de fechar o socket temos de fazer vários e indeterminados envios de mensagens para o servidor e receções.
-
-
-    // shutdown the connection since no more data will be sent
-    //iResult = shutdown(ConnectSocket, SD_SEND);
-    //if (iResult == SOCKET_ERROR) {
-    //    printf("shutdown failed with error: %d\n", WSAGetLastError());
-    //    closesocket(ConnectSocket);
-    //    WSACleanup();
-    //    return 1;
-    //}
-
-
-
-    // Receive until the peer closes the connection
+    // Recebe informação enquanto a conexão não fechar.
     do {
 
+        // Recebe informação do lado do servidor a que está conectado e guarda-a no buffer "recvbuf"
         iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+        
+        // No caso de se ter recebido algum caracter.
         if (iResult > 0) {
             printf("%.*s\n", iResult, recvbuf);
 
-            //get line from terminal
             ZeroMemory(sendbuf, DEFAULT_BUFLEN);
+
+            // Obtem do terminal o que o utilizador escreveu.
             fgets(sendbuf, DEFAULT_BUFLEN, stdin);
 
-            //Remove last character. It was a '\n'
+            // Remove o último caracter que é o '\n'.
             sendbuflen = strlen(sendbuf);
             sendbuf[sendbuflen-1] = '\0';
 
-            // Transforms all characters in the buffer to Upper Case to allow commands to be case insensitive
+            // Transforma todos os caracteres do buffer que contem a mensagem lida para "Upper Case" de forma a que os comandos sejam case insensitive.
             for (int i = 0; i < strlen(sendbuf); i++)
-            {
                 sendbuf[i] = toupper(sendbuf[i]);
-            }
 
+            // No caso do utilizador ter escrito o comando END, sai-se do ciclo while e trata-se de fechar o socket de comunicação.
             if (strcmp(sendbuf, "END") == 0)
                 break;
 
-            // Send a buffer
+            // Enviar para o servidor o buffer lido do terminal "sendbuf"
             iResult = send(ConnectSocket, sendbuf, (int)strlen(sendbuf), 0);
             if (iResult == SOCKET_ERROR) {
                 printf("send failed with error: %d\n", WSAGetLastError());
@@ -148,14 +132,14 @@ int __cdecl main(int argc, char** argv)
             }
 
         }
-        else if (iResult == 0)
+        else if (iResult == 0)// No caso do valor iResult ser 0 é porque não se leu nenhuma informação do servidor, ou seja, a conexão não está estabelecida.
             printf("Connection closed\n");
         else
             printf("recv failed with error: %d\n", WSAGetLastError());
 
     } while (iResult > 0);
 
-    // cleanup
+    // Quando se sair do ciclo é porque já não se pretende comunicar com o servidor ou porque a conexão foi fechada do lado do servidor, logo, fecha-se o socket e limpam-se possiveis recursos alocados.
     closesocket(ConnectSocket);
     WSACleanup();
 
